ec923aa4a56853338aebf991a1754b85a3bd73a6
UladzimirRashuk <rashuk.vova@gmail.com>
2022-06-21 20:14:57 +0300

    First implementation

---
+++ main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+
+    a = int(input('Input first number: '))
+    b = int(input('Input second number: '))
+    sign = input('Input operation: ')
+
+    if sign == '+':
+        print(a + b)
+    elif sign == '-':
+        print(a - b)
+    elif sign == '/':
+        print(a / b)
+    elif sign == '*':
+        print(a * b)
+    else:
+        print('unknown operation')
